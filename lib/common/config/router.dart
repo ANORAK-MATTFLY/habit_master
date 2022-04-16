2d8488a800b8036f69231742f68fc7eb3a35e4c7
DMiyalu <dieudonnemiyalu6@gmail.com>
2022-04-16 02:33:05 +0100

    add ddd architecture

---
+++ lib/common/config/router.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+
+// List<GetPage<dynamic>> getPages() {
+//   return [
+//     GetPage(
+//         name: '/', page: () => RouteStack(), transition: Transition.cupertino),
+//         GetPage(
+//         name: '/splash_screen', page: () => SplashScreen(), transition: Transition.cupertino),
+//   ];
+// }
