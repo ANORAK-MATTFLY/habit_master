4afc76e77e039edcb6a1f9b11befb6bd754d024a
ANORAKMATFLY <benmatanda354@gmail.com>
2023-01-31 20:49:52 +0100

    style: habit description section

--- lib/features/routine/presentation/pages/chart_and_desc.dart
+++ lib/features/routine/presentation/pages/chart_and_desc.dart
@@ -1,6 +1,10 @@
+import 'dart:math';
+
 import 'package:blur/blur.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:habit_master/shared/static/3d_objects.dart';
 import 'package:show_up_animation/show_up_animation.dart';
 
 import '../widgets/charts/stacked_chart.dart';
@@ -17,6 +21,8 @@ class _ChartAndDescriptionState extends State<ChartAndDescription> {
   bool isTap = false;
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final random = Random();
     return Container(
       height: 500,
       padding: const EdgeInsets.only(top: 90.0),
@@ -111,7 +117,93 @@ class _ChartAndDescriptionState extends State<ChartAndDescription> {
                     ),
                   )
                 : const Center(),
-          )
+          ),
+          Positioned(
+            bottom: 0.0,
+            child: Container(
+              height: 100.0,
+              width: MediaQuery.of(context).size.width,
+              // color: Colors.red,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    height: 70.0,
+                    width: 70.0,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.contain,
+                        image: AssetImage("assets/images/bill-gate.png"),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    height: 90,
+                    width: (width - 80),
+                    decoration: BoxDecoration(
+                      // color: Color.fromARGB(232, 181, 106, 225),
+                      gradient: LinearGradient(
+                          colors: [
+                            const Color(0xB0246776).withOpacity(0.4),
+                            const Color(0xFF855A9E).withOpacity(0.9),
+                          ],
+                          begin: Alignment.topRight,
+                          end: Alignment.bottomLeft,
+                          stops: const [0.0, 0.8],
+                          tileMode: TileMode.clamp),
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(10.0),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color.fromARGB(237, 120, 255, 239)
+                              .withOpacity(0.2),
+                          spreadRadius: 8,
+                          blurRadius: 30,
+                          offset:
+                              const Offset(2, 9), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 100.0,
+                          width: 100.0,
+                          decoration: BoxDecoration(
+                            // color: Colors.red,
+                            image: DecorationImage(
+                              fit: BoxFit.contain,
+                              image: AssetImage(threeDObjects[
+                                  random.nextInt(threeDObjects.length)]),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            padding: const EdgeInsets.all(10.0),
+                            // color: Colors.green,
+                            child: const Center(
+                                child: Text(
+                              "Bill Gates's Daily Routine has a success rate of 12%. So be focused and good luck!",
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 213, 213, 213),
+                                fontFamily: "Twitterchirp",
+                                fontSize: 13.0,
+                                fontWeight: FontWeight.w600,
+                                // overflow: TextOverflow.ellipsis,
+                              ),
+                            )),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ).animate().fadeIn(duration: 500.milliseconds),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
---
+++ lib/shared/static/3d_objects.dart
@@ -0,0 +1,7 @@
+// ignore: non_constant_identifier_names
+final threeDObjects = [
+  "assets/images/bg.png",
+  "assets/images/bubble3.png",
+  "assets/images/3d objects/Spiral-2.png",
+  "assets/images/3d objects/Polyhedron.png",
+];
